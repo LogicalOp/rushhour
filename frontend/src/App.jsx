1985906e9e89ac40154b23fc4cfa88e8647eafa9
dylan <xdylar@gmail.com>
2024-10-09 15:35:20 +0100

    Simple Vite React App setup.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline'
+import { Button, Stack } from '@mui/material/';
+import Header from './components/Header';
+import Home from './pages/Home';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const dark = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#ed4b82',
+    },
+    secondary: {
+      main: '#18181c',
+    },
+  },
+});
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ThemeProvider theme={dark}>
+        <CssBaseline />
+        <Header />
+        <Router>
+          <Routes>
+              <Route path='/' element={<Home />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
